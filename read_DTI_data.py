1d8deafddfac07c26527fb8c8ea55f96e83cfee4
Renata Prôa <72483356+renataproa@users.noreply.github.com>
2023-07-12 23:07:34 -0400

    Create read_DTI_data.py

---
+++ read_DTI_data.py
@@ -0,0 +1,18 @@
+import nibabel as nib
+
+def get_DTI_space_voxels_dims(dti_file_path):
+    """
+    Calculates the dimentions of the DTI voxel in mm3 in the diffusion space.
+    
+    Parameters: 
+    dti_file_path (str): path to a DTI compressed nii file (.nii.gz). Example: 'dti_FA.nii.gz'.
+    Returns:
+    vox_mm3 (float): voxel dimentions in mm3.
+    
+    """
+    nii_data = nib.load(dit_file_path)
+    header = nii_data.header
+    vox_dims = header.get_zooms() #get the voxel sizes in millimeters in the DTI space
+    vox_mm3 = vox_dims[0]*vox_dims[1]*vox_dims[2]
+    
+    return vox_mm3
